5b79a1d776618791c2ff9bc9da0831035eb31c05
Sutkowski Kamil <sutkowski.kamil@gmail.com>
2016-02-11 18:07:07 +0100

    introduce InMemory File Storage autoconfigure, starter and sample

---
+++ storage-connector-autoconfigure/src/main/java/pl/sutkowski/storageconnector/autoconfigure/memory/InMemoryFileStorageAutoConfiguration.java
@@ -0,0 +1,17 @@
+package pl.sutkowski.storageconnector.autoconfigure.memory;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pl.sutkowski.api.FileStorage;
+import pl.sutkowski.storageconnector.memory.InMemoryFileStorage;
+
+@Configuration
+@ConditionalOnClass(InMemoryFileStorage.class)
+public class InMemoryFileStorageAutoConfiguration {
+
+    @Bean
+    public FileStorage fileStorage() {
+        return new InMemoryFileStorage();
+    }
+}
---
+++ storage-connector-samples/storage-connector-sample-memory/src/test/java/memory/SampleInMemoryFileStorageRestControllerTest.java
@@ -0,0 +1,31 @@
+package memory;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import pl.sutkowski.api.FileStorage;
+import pl.sutkowski.storageconnector.memory.InMemoryFileStorage;
+import sample.memory.SampleInMemoryFileStorageApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SampleInMemoryFileStorageApplication.class)
+public class SampleInMemoryFileStorageRestControllerTest {
+
+    @Autowired
+    FileStorage fileStorage;
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void fileStorageShouldBeInstanceOfInMemory() {
+        Assertions.assertThat(fileStorage).isNotNull();
+        Assertions.assertThat(fileStorage instanceof InMemoryFileStorage).isTrue();
+    }
+
+}
